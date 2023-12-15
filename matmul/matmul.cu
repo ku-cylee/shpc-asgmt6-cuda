0c423d48b14d86ad10ed90010db5d57321a4a1f3
Chanyoung Lee <btstar1998@gmail.com>
2023-12-15 15:58:36 +0900

    Change TILE_SIZE -> GPU_THREADS, ITERATION -> NUM_CYCLES

--- matmul/matmul.cu
+++ matmul/matmul.cu
@@ -14,8 +14,8 @@
     }                                                                 \
   } while (0)
 
-#define TILE_SIZE     32
-#define ITERATION     16
+#define GPU_THREADS   32
+#define NUM_CYCLES    16
 
 static int mpi_rank, mpi_world_size;
 
@@ -24,20 +24,20 @@ static __global__ void matmul_kernel(float *A, float *B, float *C, int M, int N,
   int local_row = threadIdx.y;
   int local_col = threadIdx.x;
 
-  int global_row = TILE_SIZE * blockIdx.y + local_row;
-  int global_col = TILE_SIZE * blockIdx.x + local_col;
+  int global_row = GPU_THREADS * blockIdx.y + local_row;
+  int global_col = GPU_THREADS * blockIdx.x + local_col;
 
-  __shared__ float A_tile[TILE_SIZE][TILE_SIZE];
-  __shared__ float B_tile[TILE_SIZE][TILE_SIZE];
+  __shared__ float A_tile[GPU_THREADS][GPU_THREADS];
+  __shared__ float B_tile[GPU_THREADS][GPU_THREADS];
 
   float sum = 0.0f;
-  for (int k = 0; k < K; k += TILE_SIZE) {
+  for (int k = 0; k < K; k += GPU_THREADS) {
     A_tile[local_row][local_col] = A[global_row * K + local_col + k];
     B_tile[local_row][local_col] = B[(local_row + k) * N + global_col];
 
     __syncthreads();
 
-    for (int t = 0; t < TILE_SIZE; t++) {
+    for (int t = 0; t < GPU_THREADS; t++) {
       sum += A_tile[local_row][t] * B_tile[t][local_col];
     }
 
@@ -49,15 +49,15 @@ static __global__ void matmul_kernel(float *A, float *B, float *C, int M, int N,
 
 #define NGPU 4
 
-static int Mbegin[ITERATION][NGPU], Mend[ITERATION][NGPU];
+static int Mbegin[NUM_CYCLES][NGPU], Mend[NUM_CYCLES][NGPU];
 static int ngpu;
 static cudaStream_t streams[NGPU];
 static float *A_gpu[NGPU], *B_gpu[NGPU], *C_gpu[NGPU];
 
 void matmul(float *A, float *B, float *C, int M, int N, int K) {
 
-  int M_per_iter = M / ITERATION;
-  int M_per_iter_node = M_per_iter / mpi_world_size;
+  int M_per_cycle = M / NUM_CYCLES;
+  int M_per_cycle_node = M_per_cycle / mpi_world_size;
 
   MPI_Request scatter_req, gather_req;
   
@@ -69,47 +69,47 @@ void matmul(float *A, float *B, float *C, int M, int N, int K) {
   }
 
   MPI_Iscatter(
-    A, M_per_iter_node * K, MPI_FLOAT,
-    A, M_per_iter_node * K, MPI_FLOAT,
+    A, M_per_cycle_node * K, MPI_FLOAT,
+    A, M_per_cycle_node * K, MPI_FLOAT,
     0, MPI_COMM_WORLD, &scatter_req);
 
-  for (int iter = 0; iter < ITERATION; iter++) {
+  for (int cycle = 0; cycle < NUM_CYCLES; cycle++) {
     MPI_Wait(&scatter_req, MPI_STATUS_IGNORE);
 
     // Async memcpy H->D on each GPU
     for (int i = 0; i < ngpu; i++) {
       CHECK_CUDA(cudaSetDevice(i));
-      CHECK_CUDA(cudaMemcpyAsync(A_gpu[i], &A[Mbegin[iter][i] * K],
-                                (Mend[iter][i] - Mbegin[iter][i]) * K * sizeof(float),
+      CHECK_CUDA(cudaMemcpyAsync(A_gpu[i], &A[Mbegin[cycle][i] * K],
+                                (Mend[cycle][i] - Mbegin[cycle][i]) * K * sizeof(float),
                                 cudaMemcpyHostToDevice, streams[i]));
     }
 
-    if (iter + 1 != ITERATION) {
+    if (cycle + 1 != NUM_CYCLES) {
       MPI_Iscatter(
-        A + (iter + 1) * M_per_iter * K, M_per_iter_node * K, MPI_FLOAT,
-        A + (iter + 1) * M_per_iter * K, M_per_iter_node * K, MPI_FLOAT,
+        A + (cycle + 1) * M_per_cycle * K, M_per_cycle_node * K, MPI_FLOAT,
+        A + (cycle + 1) * M_per_cycle * K, M_per_cycle_node * K, MPI_FLOAT,
         0, MPI_COMM_WORLD, &scatter_req);
     }
 
     // Run kernels asynchronously on each GPU
     for (int i = 0; i < ngpu; i++) {
       CHECK_CUDA(cudaSetDevice(i));
-      dim3 blockDim(TILE_SIZE, TILE_SIZE);
-      dim3 gridDim((N + TILE_SIZE - 1) / TILE_SIZE, (Mend[iter][i] - Mbegin[iter][i] + TILE_SIZE - 1) / TILE_SIZE);
+      dim3 blockDim(GPU_THREADS, GPU_THREADS);
+      dim3 gridDim((N + GPU_THREADS - 1) / GPU_THREADS, (Mend[cycle][i] - Mbegin[cycle][i] + GPU_THREADS - 1) / GPU_THREADS);
       matmul_kernel<<<gridDim, blockDim, 0, streams[i]>>>(
-          A_gpu[i], B_gpu[i], C_gpu[i], Mend[iter][i] - Mbegin[iter][i], N, K);
+          A_gpu[i], B_gpu[i], C_gpu[i], Mend[cycle][i] - Mbegin[cycle][i], N, K);
       CHECK_CUDA(cudaGetLastError());
     }
 
-    if (iter != 0) {
+    if (cycle != 0) {
       MPI_Wait(&gather_req, MPI_STATUS_IGNORE);
     }
 
     // Async memcpy D->H on each GPU
     for (int i = 0; i < ngpu; i++) {
       CHECK_CUDA(cudaSetDevice(i));
-      CHECK_CUDA(cudaMemcpyAsync(&C[Mbegin[iter][i] * N], C_gpu[i],
-                                (Mend[iter][i] - Mbegin[iter][i]) * N * sizeof(float),
+      CHECK_CUDA(cudaMemcpyAsync(&C[Mbegin[cycle][i] * N], C_gpu[i],
+                                (Mend[cycle][i] - Mbegin[cycle][i]) * N * sizeof(float),
                                 cudaMemcpyDeviceToHost, streams[i]));
     }
 
@@ -120,8 +120,8 @@ void matmul(float *A, float *B, float *C, int M, int N, int K) {
     }
 
     MPI_Igather(
-      C + iter * M_per_iter * N, M_per_iter_node * N, MPI_FLOAT,
-      C + iter * M_per_iter * N, M_per_iter_node * N, MPI_FLOAT,
+      C + cycle * M_per_cycle * N, M_per_cycle_node * N, MPI_FLOAT,
+      C + cycle * M_per_cycle * N, M_per_cycle_node * N, MPI_FLOAT,
       0, MPI_COMM_WORLD, &gather_req);
   }
 
@@ -144,15 +144,15 @@ void matmul_initialize(int M, int N, int K) {
     printf("[rank %d] device %d: %s\n", mpi_rank, i, props[i].name);
   }
 
-  int M_per_iter = M / ITERATION;
-  int M_per_node = M_per_iter / mpi_world_size;
+  int M_per_cycle = M / NUM_CYCLES;
+  int M_per_node = M_per_cycle / mpi_world_size;
   int M_per_gpu = M_per_node / ngpu;
 
-  for (int iter = 0; iter < ITERATION; iter++) {
+  for (int cycle = 0; cycle < NUM_CYCLES; cycle++) {
     for (int i = 0; i < ngpu; i++) {
-      Mbegin[iter][i] = iter * M_per_iter + M_per_gpu * i;
-      Mend[iter][i] = Mbegin[iter][i] + M_per_gpu;
-      // if (i == ngpu - 1) Mend[iter][i] = M_per_node;
+      Mbegin[cycle][i] = cycle * M_per_cycle + M_per_gpu * i;
+      Mend[cycle][i] = Mbegin[cycle][i] + M_per_gpu;
+      // if (i == ngpu - 1) Mend[cycle][i] = M_per_node;
     }
   }
 
